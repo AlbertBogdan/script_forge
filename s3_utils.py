a6716cbfbd25f9973b7644ef6d3dd918974d5107
Albert <albert.bohdan@azati.com>
2025-01-22 12:29:46 +0300

    add output logging

--- s3_utils.py
+++ s3_utils.py
@@ -1,16 +1,17 @@
 from tqdm.auto import tqdm
-
 import boto3
 import uuid
 import logging
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 from botocore.exceptions import ClientError
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from rich.console import Console
 from rich.tree import Tree
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class S3Manager:
     def __init__(self, aws_access_key_id: str, aws_secret_access_key: str):
@@ -20,7 +21,7 @@ class S3Manager:
             aws_secret_access_key=aws_secret_access_key,
         )
 
-    def list_objects(self, bucket_name: str, prefix: str, file_extension: str = None) -> list[Path]:
+    def list_files(self, bucket_name: str, prefix: str, file_extension: str = None) -> list[Path]:
         """
         Lists objects in an S3 bucket that match the specified prefix and optional file extension.
 
@@ -33,17 +34,21 @@ class S3Manager:
             list[Path]: A list of object keys as Path objects.
         """
         try:
-            response = self.s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+            response = self.s3_client.list_objects_v2(
+                Bucket=bucket_name, Prefix=prefix)
             if "Contents" not in response:
                 logger.info(f"No objects found in folder: {prefix}")
                 return []
 
             objects = [
-                Path(obj["Key"]) for obj in response["Contents"] if not file_extension or obj["Key"].endswith(file_extension)
+                # Use PurePosixPath
+                PurePosixPath(obj["Key"]) for obj in response["Contents"]
+                if not file_extension or obj["Key"].endswith(file_extension)
             ]
 
             if not objects:
-                logger.info(f"No {file_extension if file_extension else 'files'} found in folder: {prefix}")
+                logger.info(
+                    f"No {file_extension if file_extension else 'files'} found in folder: {prefix}")
 
             return objects
 
@@ -84,8 +89,7 @@ class S3Manager:
         build_tree(folder_structure, root)
         console.print(root)
 
-
-    def download_file(self, bucket_name: str, object_key: Path, local_path: Path, keep_structure: bool = True):
+    def _download_file_single(self, bucket_name: str, object_key: Path, local_path: Path, keep_structure: bool = True, verbose: bool = True) -> bool:
         """
         Downloads a file from an S3 bucket to a local path.
 
@@ -99,21 +103,27 @@ class S3Manager:
         """
         try:
             if keep_structure:
-                full_local_path = local_path / object_key
+                full_local_path = local_path / \
+                    object_key.relative_to(object_key.anchor)
             else:
-                unique_suffix = uuid.uuid4().hex[:8]
                 full_local_path = local_path / f"{object_key.name}"
 
             full_local_path.parent.mkdir(parents=True, exist_ok=True)
 
-            self.s3_client.download_file(bucket_name, str(object_key), str(full_local_path))
-            logger.info(f"File {object_key} downloaded to {full_local_path}")
-
+            self.s3_client.download_file(
+                bucket_name, str(object_key), str(full_local_path))
+            if verbose:
+                tqdm.write(f"Downloaded: {object_key}")
+            return True
         except ClientError as e:
             logger.error(f"Error downloading file {object_key}: {e}")
+            return False
+        except Exception as e:
+            logger.error(f"Unexpected error downloading file {
+                         object_key}: {e}")
+            return False
 
-
-    def parallel_download(self, list_files: list[Path | str], bucket_name: str, local_base_path: Path, max_workers: int = 10, keep_structure=True):
+    def download_files(self, list_files: list[Path | str], bucket_name: str, local_base_path: Path, max_workers: int = 10, keep_structure=True):
         """
         Downloads multiple files in parallel from an S3 bucket.
 
@@ -122,16 +132,46 @@ class S3Manager:
             bucket_name (str): The name of the S3 bucket.
             local_base_path (Path): The base directory where files will be downloaded.
             max_workers (int): Maximum number of threads to use for parallel downloads. Defaults to 10.
+            keep_structure (bool): Whether to preserve the S3 directory structure locally.
+                    If False, adds unique identifiers to avoid name conflicts. Defaults to True.
         """
-        list_files = [Path(item) if isinstance(item, str) else item for item in list_files]
+        list_files = [Path(item) if isinstance(item, str)
+                      else item for item in list_files]
+        success_count = 0
+        failure_count = 0
 
         with ThreadPoolExecutor(max_workers) as executor:
-            futures = []
-            for file in list_files:
-                futures.append(executor.submit(self.download_file, bucket_name, file, local_base_path, keep_structure))
+            futures = [
+                executor.submit(
+                    self._download_file_single,
+                    bucket_name,
+                    file,
+                    local_base_path,
+                    keep_structure,
+                    False
+                )
+                for file in list_files
+            ]
 
-            for future in tqdm(as_completed(futures), total=len(futures)):
-                future.result()
+            with tqdm(
+                total=len(futures),
+                desc=f"Downloading {len(futures)} files",
+                unit="file",
+                dynamic_ncols=True
+            ) as pbar:
+                for future in as_completed(futures):
+                    result = future.result()
+                    if result:
+                        success_count += 1
+                    else:
+                        failure_count += 1
+                    pbar.update(1)
+
+        tqdm.write(f"\nAll files processed. Downloaded to: {local_base_path}")
+        tqdm.write(f"Successfully downloaded: {success_count} files")
+        tqdm.write(f"Failed downloads: {failure_count} files")
+
+        return success_count, failure_count
 
     def upload_file(self, bucket_name: str, object_key: Path, local_path: Path):
         """
@@ -146,7 +186,9 @@ class S3Manager:
             >>> s3_manager.upload_file('my-bucket', 'path/to/my-file.txt', '/local/path/to/my-file.txt')
         """
         try:
-            self.s3_client.upload_file(str(local_path), bucket_name, str(object_key))
-            logger.info(f"File {local_path} uploaded to {bucket_name}/{object_key}")
+            self.s3_client.upload_file(
+                str(local_path), bucket_name, str(object_key))
+            logger.info(f"File {local_path} uploaded to {
+                        bucket_name}/{object_key}")
         except ClientError as e:
             logger.error(f"Error uploading file {local_path}: {e}")
